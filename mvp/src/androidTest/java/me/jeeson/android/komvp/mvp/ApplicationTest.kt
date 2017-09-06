7ebbc74c2f25e72315203987beb42f752b29600e
Jeeson <smuwjs@163.com>
2017-09-06 11:29:51 +0800

    Add RxJava and RxLifecycle.

---
+++ mvp/src/androidTest/java/me/jeeson/android/komvp/mvp/ApplicationTest.kt
@@ -0,0 +1,15 @@
+package me.jeeson.android.komvp.mvp
+
+import android.app.Application
+import android.test.ApplicationTestCase
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+
+/**
+ * [Testing Fundamentals](http://d.android.com/tools/testing/testing_android.html)
+ */
+class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
